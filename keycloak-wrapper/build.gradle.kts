d3b73a7734c7f835e1451749601ee3bea765e871
Peter Wegner <peterwegner3141@gmail.com>
2025-02-09 02:12:33 +0100

    add keycloak client lib

--- keycloak-wrapper/build.gradle.kts
+++ keycloak-wrapper/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.keycloak:keycloak-admin-client:26.0.4")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
