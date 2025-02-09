0b13166787119498b7737353480fcab79e8f63a9
Peter Wegner <peterwegner3141@gmail.com>
2025-02-09 02:08:26 +0100

    initial backend

---
+++ keycloak-wrapper/src/main/kotlin/com/example/keycloak_wrapper/KeycloakWrapperApplication.kt
@@ -0,0 +1,11 @@
+package com.example.keycloak_wrapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeycloakWrapperApplication
+
+fun main(args: Array<String>) {
+	runApplication<KeycloakWrapperApplication>(*args)
+}
---
+++ keycloak-wrapper/src/test/kotlin/com/example/keycloak_wrapper/KeycloakWrapperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.keycloak_wrapper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeycloakWrapperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
