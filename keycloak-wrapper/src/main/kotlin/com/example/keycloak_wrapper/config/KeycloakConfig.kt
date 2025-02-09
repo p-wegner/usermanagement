8ffcf9c23a3aebd508f63d4536ef6de44212d504
Peter Wegner <peterwegner3141@gmail.com>
2025-02-09 02:18:53 +0100

    feat: Add Keycloak configuration and base configuration class

---
+++ keycloak-wrapper/src/main/kotlin/com/example/keycloak_wrapper/config/KeycloakConfig.kt
@@ -0,0 +1,32 @@
+package com.example.keycloak_wrapper.config
+
+import org.keycloak.admin.client.Keycloak
+import org.keycloak.admin.client.KeycloakBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class KeycloakConfig {
+    @Value("\${keycloak.auth-server-url}")
+    private lateinit var authServerUrl: String
+
+    @Value("\${keycloak.realm}")
+    private lateinit var realm: String
+
+    @Value("\${keycloak.resource}")
+    private lateinit var clientId: String
+
+    @Value("\${keycloak.credentials.secret}")
+    private lateinit var clientSecret: String
+
+    @Bean
+    fun keycloak(): Keycloak {
+        return KeycloakBuilder.builder()
+            .serverUrl(authServerUrl)
+            .realm(realm)
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .build()
+    }
+}
