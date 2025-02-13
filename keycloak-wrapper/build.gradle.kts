5766a6d8927078d86fe06a29fda54aae4c897003
Peter Wegner <peterwegner3141@gmail.com>
2025-02-14 00:36:45 +0100

    remove nonexistent dependency

--- keycloak-wrapper/build.gradle.kts
+++ keycloak-wrapper/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.keycloak:keycloak-admin-client:26.0.4")
-    implementation("org.keycloak:keycloak-spring-boot-starter:26.0.4")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
     implementation("org.springframework.boot:spring-boot-docker-compose")
