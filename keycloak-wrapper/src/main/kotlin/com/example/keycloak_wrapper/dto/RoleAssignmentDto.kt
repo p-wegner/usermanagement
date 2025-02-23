5ad7ac0eccf97f5f76f60ec06d51f98b243c5c60
Peter Wegner <peterwegner3141@gmail.com>
2025-02-23 17:24:54 +0100

    feat: Add `allRoleIds` property to `RoleAssignmentDto`

--- keycloak-wrapper/src/main/kotlin/com/example/keycloak_wrapper/dto/RoleAssignmentDto.kt
+++ keycloak-wrapper/src/main/kotlin/com/example/keycloak_wrapper/dto/RoleAssignmentDto.kt
@@ -3,4 +3,8 @@ package com.example.keycloak_wrapper.dto
 data class RoleAssignmentDto(
     val realmRoleIds: List<String> = emptyList(),
     val clientRoleIds: Map<String, List<String>> = emptyMap()
-)
+) {
+    val allRoleIds: List<String> get() {
+        return realmRoleIds + clientRoleIds.values.flatten()
+    }
+}
